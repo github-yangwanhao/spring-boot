b7a47e556e29bfbe0f33a97117011dafa3dba3f3
杨万浩 <yangwanhao163@163.com>
2021-08-05 13:43:47 +0800

    字符串加解密单元测试

--- spring-boot-test/src/test/java/cn/yangwanhao/boot/springboot/test/SpringBootTestApplicationTests.java
+++ spring-boot-test/src/test/java/cn/yangwanhao/boot/springboot/test/SpringBootTestApplicationTests.java
@@ -1,13 +1,32 @@
 package cn.yangwanhao.boot.springboot.test;
 
+import org.jasypt.encryption.StringEncryptor;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
+import lombok.extern.slf4j.Slf4j;
+
+@RunWith(SpringRunner.class)
 @SpringBootTest
-class SpringBootTestApplicationTests {
+@Slf4j
+public class SpringBootTestApplicationTests {
+
+    @Autowired
+    private StringEncryptor encryptor;
 
     @Test
-    void contextLoads() {
+    public void testEncryptor() {
+        /*
+         * 深渊巨坑 springboot 2.x junit test 不支持yml配置文件 必须用properties文件
+         */
+        String originStr = "******";
+        String encryptStr = encryptor.encrypt(originStr);
+        String decryptStr = encryptor.decrypt(encryptStr);
+        log.info("加密后的字符串:{}", encryptStr);
+        log.info("解密后的字符串:{}", decryptStr);
     }
 
 }
